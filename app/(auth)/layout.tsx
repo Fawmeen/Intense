87b99d2084611028637e36e791a0618a1fbbac1a
fawmeen <mohamedfawmeen76@gmail.com>
2025-07-26 16:06:19 +0530

    Hajimemashou

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import {ReactNode} from 'react'
+
+const authlayout = ({children}: {children: ReactNode}) => {
+  return (
+    <div className="auth-layout">{children}</div>
+  )
+}
+
+export default authlayout     
+
+
+// 28:38m timestamp
