506c8228118cd41cd3fec64c4269114c26eb63da
fawmeen <mohamedfawmeen76@gmail.com>
2025-07-29 14:55:03 +0530

    ---- AUTH ERROR -----

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,25 +1,26 @@
-import {ReactNode} from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { isAuthenticated } from '@/lib/auction/auth.action'
-import { redirect } from 'next/navigation'
-const layout = async({children}:{children : ReactNode}) => {
-   const isUserAuthenticated = await isAuthenticated();
+import Link from "next/link";
+import Image from "next/image";
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
 
-   if(!isUserAuthenticated) redirect('/SignIn')
+import { isAuthenticated } from "@/lib/auction/auth.action";
+
+const Layout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) redirect("/sign-in");
+
   return (
     <div className="root-layout">
       <nav>
         <Link href="/" className="flex items-center gap-2">
-         <Image src = "/logo.svg" alt="logo" width={38} height={32} />
-         <h2 className="text-primary-100">Intense</h2>
-         
+          <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
+          <h2 className="text-primary-100">PrepWise</h2>
         </Link>
-        </nav>
-      {children}
-    
-    </div>
-  )
-}
+      </nav>
 
-export default layout
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
