a880976b64a49542a8c87b700b11b2d3e7bf5a10
fawmeen <mohamedfawmeen76@gmail.com>
2025-08-10 22:00:47 +0530

    interview updated dynamically

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,9 +4,21 @@ import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { dummyInterviews } from '@/constants';
 import InterviewCard from '@/components/InterviewCard';
-const page = () => {
+import { getCurrentUser, getInterviewByUserId, getLatestInterviews } from '@/lib/auction/auth.action';
+const page = async () => {
   const head = "Get Intense Training on Interview with your Agent";
   const desc = "practice on real interview questions & evaluate your skills";
+
+  const user = await getCurrentUser();
+
+  const[userInterviews, latestInterviews ] = await Promise.all([
+   await getInterviewByUserId(user?.id!),
+   await getLatestInterviews({userId: user?.id!})
+  ])
+ const hasPastInterviews = userInterviews?.length > 0;
+ const hasUpcomingInterviews = latestInterviews?.length > 0;
+ 
+
   return (
    <>
    <section className="card-cta">
@@ -23,20 +35,31 @@ const page = () => {
 
    <section className="flex flex-col gap-6 mt-8">
       <h2>Your Interviews</h2>
-      <div className="interview-section">
-         {dummyInterviews.map((interview) =>(
-          <InterviewCard {...interview} key = {interview.id} />
-         ))}
-         {/* <p>{head}</p> */}
+      <div className="interviews-section">
+         {
+             hasPastInterviews ? (
+               userInterviews?.map((interview) => (
+               <InterviewCard {...interview} key = {interview.id} />
+               ))) : (
+                  <p>You haven't taken any interview yet</p> 
+               )
+             
+            }
+         
       </div>
    </section>
    
    <section className="flex flex-col gap-6 mt-8">
              <h2>Take an Interview</h2>
-             <div className = "interview-section">
-                {dummyInterviews.map((interview) =>(
-                     <InterviewCard {...interview} key={interview.id} />
-         ))}
+             <div className = "interviews-section">
+                {
+                hasUpcomingInterviews? (
+               latestInterviews?.map((interview) => (
+               <InterviewCard {...interview} key = {interview.id} />
+               ))) : (
+                  <p>There are no new interview available yet</p> 
+               )
+         }
              </div>
    </section>
    </>
--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -1,15 +1,12 @@
-import { request } from "http";
-import { google } from "@ai-sdk/google";
 import { generateText } from "ai";
-import { getRandomInterviewCover } from "@/lib/utils";
-import { db } from "@/Firebase/admin";
+import { google } from "@ai-sdk/google";
 
-export async function GET() {
-  return Response.json({ success: true, date: "THANK YOU!" }, { status: 200 });
-}
+import { db } from "@/Firebase/admin";
+import { getRandomInterviewCover } from "@/lib/utils";
 
 export async function POST(request: Request) {
   const { type, role, level, techstack, amount, userid } = await request.json();
+
   try {
     const { text: questions } = await generateText({
       model: google("gemini-2.0-flash-001"),
@@ -23,15 +20,16 @@ export async function POST(request: Request) {
         The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
         Return the questions formatted like this:
         ["Question 1", "Question 2", "Question 3"]
+        
         Thank you! <3
-      `,
+    `,
     });
 
     const interview = {
-      role,
-      type,
-      level,
-      techstack: techstack.split(","), // ✅ fixed
+      role: role,
+      type: type,
+      level: level,
+      techstack: techstack.split(","),
       questions: JSON.parse(questions),
       userId: userid,
       finalized: true,
@@ -43,10 +41,11 @@ export async function POST(request: Request) {
 
     return Response.json({ success: true }, { status: 200 });
   } catch (error) {
-    console.error(error);
-    return Response.json(
-      { success: false, error: "An error occurred while processing your request." },
-      { status: 500 }
-    );
+    console.error("Error:", error);
+    return Response.json({ success: false, error: error }, { status: 500 });
   }
+}
+
+export async function GET() {
+  return Response.json({ success: true, data: "Thank you!" }, { status: 200 });
 }
